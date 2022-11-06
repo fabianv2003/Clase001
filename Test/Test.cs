64c39fa76b7331492e8d411bcf2ccd3c80ad1134
fabian <fabianv2003@gmail.com>
2022-11-05 23:04:26 -0300

    Agregar archivos de proyecto.

---
+++ Test/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase001.Test
+{
+    internal class Test
+    {
+    }
+}
